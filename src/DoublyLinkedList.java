037ee938c739b22679f90f05d3fa968c97eff1c4
Hip <cnhh85@gmail.com>
2021-10-31 22:10:38 +0700

    finish doubly linked list class

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,3 +1,47 @@
 public class DoublyLinkedList<E> {
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    public DoublyLinkedList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty())
+            return null;
+        return header.getNext().getElement();
+    }
+
+    public E last() {
+        if (isEmpty())
+            return null;
+        return trailer.getPrev().getElement();
+    }
+
+    public void addFirst(E e) {
+        addBetween(e, header, header.getNext());
+    }
+
+    public void addLast(E e) {
+        addBetween(e, trailer.getPrev(), trailer);
+    }
+
+    private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
+        Node<E> newest = new Node<>(e, predecessor, successor);
+        predecessor.setNext(newest);
+        successor.setPrev(newest);
+        size++;
+    }
 
 }
