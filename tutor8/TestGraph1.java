dc88622fe5ebecf43f6e473d98100487dc7ef33b
Hip <cnhh85@gmail.com>
2021-10-31 21:18:20 +0700

    finish tutor8

--- tutor8/TestGraph1.java
+++ tutor8/TestGraph1.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class TestGraph1 {
-    
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g = g.loadFromFile("graph1.txt");
+        System.out.println(g.toString());
+        ArrayList<Edge> edges = g. depthFirstSearch();
+        System.out.println("Depth First Search: ");
+        g.printEdges(edges, System.out);
+        edges = g.breadFirstSearch();
+        System.out.println("Bread First Search: ");
+        g.printEdges(edges, System.out);
+    }
 }
