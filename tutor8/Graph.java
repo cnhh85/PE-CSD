bd49db5b48a9e66ab0a89550caa06f053899d08d
Hip <cnhh85@gmail.com>
2021-10-31 21:14:28 +0700

    finish class Graph

--- tutor8/Graph.java
+++ tutor8/Graph.java
@@ -84,4 +84,96 @@ public class Graph extends TreeSet<Vertex> {
         }
         return g;
     }
+
+    public String toString() {
+        String S = "Empty graph";
+        if (this == null || this.size() == 0)
+            return S;
+        S = "Graph:\n";
+        Iterator<Vertex> vertices = this.iterator();
+        while (vertices.hasNext()) {
+            Vertex u = vertices.next();
+            S += u.getName();
+            Iterator<Edge> edges = u.getAdjList().iterator();
+            while (edges.hasNext()) {
+                Edge e = edges.next();
+                S += "(" + e.u.getName() + ", " + e.v.getName() + ", " + e.weight + ")" + " ";
+            }
+            S += "\n";
+        }
+        return S;
+    }
+
+    protected int DFS(Vertex u, int order, ArrayList<Edge> edges) {
+        int newOrder = order + 1;
+        u.setNum(order);
+        Iterator<Edge> it = u.getAdjList().iterator();
+        while (it.hasNext()) {
+            Edge e = it.next();
+            Vertex v = e.getV();
+            if (v.num == 0) {
+                edges.add(e);
+                newOrder = DFS(v, newOrder, edges);
+            }
+        }
+        return newOrder;
+    }
+
+    public ArrayList<Edge> depthFirstSearch() {
+        ArrayList result = new ArrayList<Edge>();
+        Object[] vertices = this.toArray();
+        for (int i = 0; i < vertices.length; i++) {
+            Vertex v = (Vertex) vertices[i];
+            v.num = 0;
+        }
+        Integer order = 1;
+        for (int i = 0; i < vertices.length; i++) {
+            Vertex v = (Vertex) vertices[i];
+            if (v.num == 0)
+                order = DFS(v, order, result);
+        }
+        return result.size() > 0 ? result : null;
+    }
+
+    public ArrayList<Edge> breadFirstSearch() {
+        ArrayList result = new ArrayList<Edge>();
+        MyQueue<Vertex> queue = new MyQueue<Vertex>();
+        Object[] vertices = this.toArray();
+        for (int i = 0; i < vertices.length; i++) {
+            Vertex v = (Vertex) vertices[i];
+            v.num = 0;
+        }
+        int order = 1;
+
+        for (int i = 0; i < vertices.length; i++) {
+            Vertex u = (Vertex) vertices[i];
+            if (u.num == 0) {
+                u.num = order++;
+                queue.enqueue(u);
+                while (!queue.isEmpty()) {
+                    u = queue.dequeue();
+                    Iterator<Edge> it = u.adjList.iterator();
+                    while (it.hasNext()) {
+                        Edge e = it.next();
+                        Vertex v = e.getV();
+                        if (v.num == 0) {
+                            v.num = order++;
+                            queue.enqueue(v);
+                            result.add(e);
+                        }
+                    }
+                }
+            }
+        }
+        return result.size() > 0 ? result : null;
+    }
+
+    public void printEdges(ArrayList<Edge> edges, PrintStream pw) {
+        if (edges == null || edges.isEmpty()) {
+            pw.println("No edges.");
+        } else {
+            pw.println("Set of edges: ");
+            pw.println(edges);
+        }
+    }
 }
