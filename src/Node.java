f4d5c806a84afe10f721cdf37a6ff104bf4a8879
Hip <cnhh85@gmail.com>
2021-10-31 22:04:32 +0700

    add node.java

---
+++ src/Node.java
@@ -0,0 +1,36 @@
+public class Node<E> {
+    private E element;
+    private Node<E> prev;
+    private Node<E> next;
+
+    public Node(E e, Node<E> p, Node<E> n) {
+        element = e;
+        prev = p;
+        next = n;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    public void setElement(E element) {
+        this.element = element;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+}
